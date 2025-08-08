a4a45da54c4bb917bc978d061036b754c071408b
Robin <mariarobin895@gmail.com>
2025-08-08 09:15:26 +0530

    fixes for navbar

--- qtify/src/component/Button/button.js
+++ qtify/src/component/Button/button.js
@@ -3,9 +3,12 @@ import { Button, IconButton, Stack,Typography } from "@mui/material";
 import styles from "./button.module.css";
 
 export default function button({btnText,targetComponent}) {
- return (<button className={styles.feedbackButton} onClick={targetComponent}>
-         <Typography className={styles.buttonText}>{btnText}</Typography>
-         </button>);
+ return (
+ <button className={styles.feedbackButton} onClick={targetComponent}>
+<div className={styles.buttonText}>{btnText}</div>
+ </button>
+         
+);
  }
 
 
